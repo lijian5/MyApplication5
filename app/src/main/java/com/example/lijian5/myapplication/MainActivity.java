7e69e16d23330fa016bfcfa1a667595b0ecca891
lijian5 <18321674096@163.com>
2019-04-15 20:36:15 +0800

    图片加载

--- app/src/main/java/com/example/lijian5/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lijian5/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mIv4 = findViewById(R.id.image1);
         Glide.with(this).load("http://pic69.nipic.com/file/20150608/9252150_134415115986_2.jpg").into(mIv4);
     }
